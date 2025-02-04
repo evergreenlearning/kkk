75f9355c9493a63d1a088c95d625b91c2f46a98b
闲了没事敲敲键盘 <55321084+evergreenlearning@users.noreply.github.com>
2025-02-04 09:05:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 from langchain.chains import ConversationChain
 from langchain_community.chat_models import MoonshotChat, ChatTongyi
 from langchain_ollama.chat_models import ChatOllama
+import os
+os.environ["MOONSHOT_API_KEY"] = "sk-T009onh3gF4BQonapjO6KFV6CDxsV8XIMaPP1sj13eNTw0oe"
 def get_chat_response(prompt, model, memory):
-    llm = ChatOllama(model=model)
+    # llm = ChatOllama(model=model)
+    llm = MoonshotChat()
     chain=ConversationChain(llm=llm, memory=memory)
     response=chain.invoke({"input":prompt})
     return response["response"]
